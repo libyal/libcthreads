f951c9ac82807787f29b340c91ff858e40cf83b2
Joachim Metz <joachim.metz@gmail.com>
2016-09-12 07:49:03 +0200

    Worked on Windows test scripts.

--- synclibs.ps1
+++ synclibs.ps1
@@ -1,9 +1,9 @@
 # Script that synchronizes the local library dependencies
 #
-# Version: 20160318
+# Version: 20160912
 
 $GitUrlPrefix = "https://github.com/libyal"
-$LocalLibs = @("libcerror","libcstring")
+$LocalLibs = "libcerror libcstring" -split " "
 
 foreach (${LocalLib} in ${LocalLibs})
 {
