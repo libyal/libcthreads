4adc069f3eaea733d049b980d74fb08b1d6ab701
Joachim Metz <joachim.metz@gmail.com>
2026-06-21 18:42:49 +0200

    Worked on tests and changes for CI tests

--- include/libcthreads/error.h
+++ include/libcthreads/error.h
@@ -82,7 +82,7 @@ enum LIBCTHREADS_ARGUMENT_ERROR
 	 */
 	LIBCTHREADS_ARGUMENT_ERROR_UNSUPPORTED_VALUE		= 8,
 
-	/* The argument contains a value that conficts with another argument
+	/* The argument contains a value that conflicts with another argument
 	 */
 	LIBCTHREADS_ARGUMENT_ERROR_CONFLICTING_VALUE		= 9
 };
@@ -214,7 +214,7 @@ enum LIBCTHREADS_OUTPUT_ERROR
 {
 	LIBCTHREADS_OUTPUT_ERROR_GENERIC			= 0,
 
-	/* There is insuficient space to write the output
+	/* There is insufficient space to write the output
 	 */
 	LIBCTHREADS_OUTPUT_ERROR_INSUFFICIENT_SPACE		= 1
 };
